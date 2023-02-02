cefcee7b6d338439925f12c32f530deebe107ef5
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-02 19:47:24 +0100

    [algo_euclide] add a way to set a the biggest between a and b

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ def ecrire_division_euclidienne(x, y):
     resultat = str(x) + " = " + str(y) + " x " + str(quotient) 
     resultat += " + " + str(reste)
     return resultat
+
+def algorithme_euclide(a, b):
+    if (a == b):
+        return a
+    elif (b > a):
+        a += b
+        b = a - b
+        a = a - b
+    return a
