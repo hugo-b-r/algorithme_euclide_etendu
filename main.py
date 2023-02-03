07ae39013ef10b1dbbc4bec9c66ebd284dfd8562
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-03 20:56:38 +0100

    fonction pour ecrire une brique

--- main.py
+++ main.py
@@ -4,11 +4,63 @@ def calculer_division_euclidienne(x, y):
     return (quotient, reste)
     
 
+
+
 def ecrire_division_euclidienne(x, y, quotient, reste):
     texte = str(x) + " = " + str(y) + " x " + str(quotient) 
     texte += " + " + str(reste)
     return texte
 
+
+
+
+# on peut creer une sorte de brique de construction pour les calculs
+# mathématiques
+
+class Brique:
+
+    def __init__(self, termes = [], type = "rien"):
+        self.termes = termes
+        self.type = type
+
+
+
+    
+    def ecrire(self, brique):
+
+        texte = ""
+        elements_a_supprimer = []
+        for partie in brique.termes:
+            if (type(partie) == int):
+                if (partie <= 0):
+                    texte += "("
+                    texte += str(partie)
+                    texte += ")"
+                else:
+                    texte += str(partie)
+
+            else:
+                texte += "("
+                texte += partie.ecrire(partie)
+                texte += ")"
+            
+            if (brique.type == "somme"):
+                    texte += " + "
+            elif (brique.type == "produit"):
+                    texte += " x "
+
+        texte = texte[:-3]
+        return texte
+
+
+
+                
+
+#difference1 = Brique([2, -3, -6], "somme")
+#difference2 = Brique([4], "somme")
+#difference3 = Brique([3], "somme")
+#calculus = Brique([difference1, 4, 3], "produit")
+#print(calculus.ecrire(calculus.developpe()))
 
 
 
@@ -34,4 +86,7 @@ def algorithme_euclide(a, b):
         a = b
         b = reste
     return texte
-print(algorithme_euclide(19, 11))
+#print(algorithme_euclide(198, 3100))
+
+
+
