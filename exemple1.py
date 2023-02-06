ed055a92564e92052d3ebe9f0fc67876c6840e34
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-06 20:59:14 +0100

    repare exemple1.py

--- exemple1.py
+++ exemple1.py
@@ -1,12 +1,16 @@
 from bibliotheque import *
 
+a = 54
+b = 7
+division_euclidienne = calculer_division_euclidienne(a, b)
 
 print(
-    ecrire_division_euclidienne(
-        calculer_division_euclidienne(
-            54,
-            7
-        )
+    ecrire_division_euclidienne
+    (
+        a,
+        b,
+        division_euclidienne[0],
+        division_euclidienne[1]
     )
 )
 
